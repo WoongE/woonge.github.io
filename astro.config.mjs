068e092fd941e9b16527940b1f471705917fa7a0
WoongE <woong17@naver.com>
2026-01-29 01:47:35 +0900

    deploy.yml 변경: 깃헙에서 설정값을 불러와서 site 주소를 오버라이드 해서 사용하는 방식으로 변경

--- astro.config.mjs
+++ astro.config.mjs
@@ -6,6 +6,6 @@ import { defineConfig } from 'astro/config';
 
 // https://astro.build/config
 export default defineConfig({
-	site: 'https://woonge.github.io',
-	integrations: [mdx(), sitemap()],
+    site: 'https://woonge.github.io', // 실제 배포시 이 값을 사용하지 않고 github 설정에서 주소값을 가져와서 사용
+    integrations: [mdx(), sitemap()],
 });
